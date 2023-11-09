ca1ac61595d130df9ae785c9c7cb74425c872874
newbieduck05 <kwoos22281730@gmail.com>
2023-11-09 18:41:07 +0900

    Initial commit

---
+++ GameServer/GameServer.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>    
+using namespace std;
+
+#pragma commet(lib, "Ws2_32.lib")
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+int main()
+{
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	WSAData wsaData;
+
+	if (WSAStartup(wVersionRequested, &wsaData) != 0)
+	{
+		printf("WSAStartup failed with error\n");
+		return 1;
+	};
+
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		printf("socket function failed with error : %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKADDR_IN service;
+	memset(&service, 0, sizeof(service));
+	service.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
+	service.sin_port = htons(7777);
+
+	if (bind(listenSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
+	{
+		printf("bind failed with error %d\n", WSAGetLastError());
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+
+
+
+	closesocket(listenSocket);
+
+	WSACleanup();
+}
